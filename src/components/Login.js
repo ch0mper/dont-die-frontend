ca2ccf372b9ad3bfd8026eaf46ad0417e3e61b01
ch0mper <hannahchang17@gmail.com>
2018-12-20 14:40:06 -0600

    added a login form that doesnt work yet

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+
+  // fetch(api/user/auth or /api/user/login), {
+  //   method: "POST",
+  //   body:JSON.stringify( userfield.value, pwfield.value )
+  // }
+
   render() {
     return(
       <div>
-        login hi
+        <form onSubmit={this.login}>
+            <h2>Login</h2>
+            <div className="form-group">
+                <label>Email </label>
+                <input name="emailInput" className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+                <label>Password </label>
+                <input name="passwordInput" className="form-control" type="password" />
+            </div>
+            <button  className="btn btn-secondary">Login</button>
+        </form>
       </div>
     )
   }
+
+  login = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/api/v1/auth/',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            email: e.target.emailInput.value,
+            password: e.target.passwordInput.value
+        })
+    })
+        .then(res => res.json())
+        .then( result => {
+            localStorage.setItem('token', result.token)
+        })
+  }
+
 }
 
 export default Login;
