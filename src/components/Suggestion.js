7a47d76b24de4283ef31f8155ebfe99e8caa5a15
Megan Farley <megfarley4@gmail.com>
2019-01-03 21:19:55 -0600

    suggestion stuff. not finished

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class Suggestion extends Component {
+
+  state = {
+    vaccineIds: [],
+    allVaccines: [],
+    neededVaccines: []
+   }
+
+  componentDidMount(){
+    this.getVaccineIds()
+    this.fetchAllVaccines()
+  }
+
+  // fetchAllVaccines = () => {
+  //   return fetch(`http://localhost:5000/api/vaccines/`, {
+  //     headers: {
+  //       Authorization: `Bearer ${localStorage.getItem('token')}`
+  //     }
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(allVaccines => this.setState({allVaccines}))
+  // }
+
+  getVaccineIds = () => {
+    let vaccineIds = this.props.vaccines.map(vaccine => vaccine._id)
+    console.log("vaccines?????", this.props.vaccines)
+    console.log("vaccine ids?????", vaccineIds)
+    this.setState({vaccineIds})
+  }
+
+  neededVaccines = () => {
+    let neededVaccines = this.state.allVaccines.map(
+      vaccine => {
+      if(!this.state.vaccineIds.includes(vaccine._id)){
+        return vaccine
+      }
+    })
+    this.setState({neededVaccines})
+  }
+
   render() {
-    console.log(this.props.vaccines)
+    // console.log("prof vaccs", this.props.vaccines)
+    // console.log("all vaccs", this.state.allVaccines)
+    // console.log("vaccine ids", this.state.vaccineIds)
+    // console.log("needed vaccs", this.state.neededVaccines)
+
     return(
       <div>
         Upcoming vaccines and boosters!
